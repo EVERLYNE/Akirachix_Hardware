ac9325817a013a1061299564fac998eee50c1a46
Evelyne Akoth <evelyneoakoth@gmail.com>
2018-01-27 18:02:42 +0300

    first commmit

---
+++ Everlyne_cpp.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+int addition(int x,int y){int z=x+y;
+return z;
+}
+
+int main()
+{
+    int a;
+    int b;
+    cout<<"Enter first number";
+    cin>>a;
+    cout<<"Enter second number";
+    cin>>b;
+    cout<<a;cout<<" + ";cout<<b;cout<<" = ";cout<< addition(a,b);
+
+    return 0;
+}
+
+
